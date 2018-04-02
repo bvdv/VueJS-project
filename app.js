f10098db2b7e4eea7898672784b5abfc3ba2689e
bvdv <bvdv2019@gmail.com>
2018-04-02 14:32:13 +0300

    DOM methods

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+
+
 new Vue({
-  el: '#app',
-  data: {
-    title: 'Hi'
+    el: '#app',
+    data: {
+      counter: 0,
+      x: 0,
+      y: 0
+  },
+  methods: {
+    incr: function() {
+      this.counter++;
+    },
+    MouseCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    }
   }
-})
+});
