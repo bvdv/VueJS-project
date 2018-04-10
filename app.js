d8ab6f349213bf83d03b27a7a52371ff93687d52
bvdv <bvdv2019@gmail.com>
2018-04-10 22:13:48 +0300

    Refs

--- app.js
+++ app.js
@@ -5,11 +5,26 @@ var vm1 = new Vue({
     data: {
       title: 'Title1'
     },
+    methods: {
+      show: function() {
+        this.showPar = true;
+        this.updateTitle('The VueJS Obj(Updated)');
+        console.log(this.$refs)
+      },
+      updateTitle: function(title) {
+        this.title = title;
+      },
+    }
 });
 
+vm1.newProp = 'new';
+//console.log(vm1.$data.title);
+
+/*
 setTimeout( function() {
   vm1.title = 'Title1';
 }, 3000);
+*/
 
 var vm2 = new Vue({
   el: '#app2',
