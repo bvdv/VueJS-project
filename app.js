15fa70f592fe3dc2a7a0841c82536f2de3aec732
bvdv <bvdv2019@gmail.com>
2018-04-12 21:22:47 +0300

    Different component use

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-Vue.component('comp', {
+var data = { status: 'Danger!' };
+
+var st_comp = {
   data: function() {
     return {
       status: 'Normal'
     }
+    
   },
-  template: '<p>Server Stat: {{ status }} </p>'
-});
+  template: '<p>Server Stat: {{ status }} <button @click="chSt">Click!</button></p>',
+  methods: {
+    chSt: function() {
+      this.status = 'Warn!';
+    }
+  }
+};
 
 new Vue({
   el:'#app',
+  components: {
+    'comp':st_comp
+  }
+  
+})
+ 
+new Vue({
+  el:'#app2',
   
 })
