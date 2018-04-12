1d97fdb3e07aeae82ee3183616489fbedc529309
bvdv <bvdv2019@gmail.com>
2018-04-12 20:59:32 +0300

    VueJS component

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-
-
-var vm1 = new Vue({
-  el: '#app',
-  data: {
-    title: 'The VueJS Instance',
-    showParagraph: false
-  },
-  methods: {
-    show: function() {
-      this.showParagraph = !this.showParagraph;
-      this.updateTitle('The VueJS Instance (Updated)');
-      this.$refs.myButton.innerText = 'test';
-      this.$refs.heading.innerText = 'test';
-    },
-    updateTitle: function(title) {
-      this.title = title;
+Vue.component('comp', {
+  data: function() {
+    return {
+      status: 'Normal'
     }
   },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      //alert('Title changed, new value: ' + value);
-    }
-  }
+  template: '<p>Server Stat: {{ status }} </p>'
 });
 
-Vue.component('hello', {
-  template: '<h1>Hello!</h1>'
-});
+new Vue({
+  el:'#app',
+  
+})
